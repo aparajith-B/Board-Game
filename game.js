dd4de55111e87f2e9d3dc074404fb85ab2490bc9
aparajith-B <aparajith.badrinarayanan@gmail.com>
2026-02-09 18:57:19 +0530

    game.js

--- game.js
+++ game.js
@@ -29,10 +29,6 @@ function setLevel(lvl, btn){
 }
 
 function startGame(){
-    p1Score = 0;
-    p2Score = 0;
-    p1Combo = 1;
-    p2Combo = 1;
     document.getElementById("p1Score").innerText = "0";
     document.getElementById("p2Score").innerText = "0";
     document.getElementById("levelNum").innerText = selectedLevel;
@@ -141,3 +137,59 @@ function endRound(){
 function openGitHub(){
     window.open("https://github.com/aparajith-B/Board-Game.git","_blank");
 }
+
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray = [];
+
+class Particle{
+    constructor(){
+        this.x = Math.random()*canvas.width;
+        this.y = Math.random()*canvas.height;
+        this.size = Math.random()*2 + 1;
+        this.speedX = Math.random()*0.5 - 0.25;
+        this.speedY = Math.random()*0.5 - 0.25;
+    }
+
+    update(){
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        if(this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+        if(this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+    }
+
+    draw(){
+        ctx.fillStyle = "cyan";
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.size,0,Math.PI*2);
+        ctx.fill();
+    }
+}
+
+function init(){
+    for(let i=0;i<120;i++){
+        particlesArray.push(new Particle());
+    }
+}
+
+function animate(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    particlesArray.forEach(p=>{
+        p.update();
+        p.draw();
+    });
+    requestAnimationFrame(animate);
+}
+
+init();
+animate();
+
+window.addEventListener("resize",()=>{
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
